5ac6be477a31764945fae22915885c7945939edd
Joy <joyyujiepeng@gmail.com>
2023-12-20 19:23:01 +0000

    UI: add shoes background img

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -52,7 +52,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-cover bg-center ">
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-cover bg-hero bg-center ">
         <img
           src={bigShoeImg}
           alt="shoe colletion"
