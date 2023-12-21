450239bfa750e4f9f225f7c50ccbbb00ffc0313c
Joy <joyyujiepeng@gmail.com>
2023-12-21 18:15:01 +0000

    feat: finish subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,5 +1,30 @@
+import Button from "../components/Button"
+
 const Subscribe = () => {
-  return <div>Subsribe</div>
+  return (
+    <>
+      <section
+        id="contact-us"
+        className="max-container flex justify-between items-center max-lg:flex-col gap-10 "
+      >
+        <h3 className="z-10 font-palanquin text-4xl max-sm:text-[36px] max-sm:leading-[41px] font-bold">
+          Sign Up from{" "}
+          <span className="text-coral-red inline-block"> Updates</span> &
+          Newsletter
+        </h3>
+        <div className="w-full lg:max-w-[50%] flex items-center max-sm:flex-col gap-5 p-2.5 sm:border-slate-gray rounded-full">
+          <input
+            className="input"
+            type="text"
+            placeholder="subscribe@nike.com"
+          />
+          <div className="flex items-center max-sm:w-full">
+            <Button label="Sign Up" fullWidth />
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
 
 export default Subscribe
