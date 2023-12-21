8d03805134d46aef6695aaefdaa7f27a510d95d6
Joy <joyyujiepeng@gmail.com>
2023-12-21 17:51:43 +0000

    feat: add header to customer review section

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -3,11 +3,21 @@ import { reviews } from "../constants"
 
 const CustomerReviews = () => {
   return (
-    <section className="max-container gap-40 justify-center flex flex-1 max-lg:flex-col  max-lg:gap-20">
-      {reviews.map((review, idx) => {
-        return <CustomerCard key={idx} {...review} />
-      })}
-    </section>
+    <>
+      <h3 className="z-10 font-palanquin text-center text-4xl font-bold">
+        What Our <span className="text-coral-red inline-block">Customers</span>{" "}
+        Say?
+      </h3>
+      <p className="m-auto mt-4 max-w-lg text-center info-text">
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
+      <section className="max-container mt-24 gap-14 justify-evenly flex flex-1 max-lg:flex-col  max-lg:gap-20">
+        {reviews.map((review, idx) => {
+          return <CustomerCard key={idx} {...review} />
+        })}
+      </section>
+    </>
   )
 }
 
